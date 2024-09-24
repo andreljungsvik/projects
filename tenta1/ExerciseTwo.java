eabcae27ae76b09107b5473ccd8ccb498cf4c2fd
andreljungsvik <andre.ljungsvik@gmail.com>
2024-09-24 19:42:40 +0200

    tenta

---
+++ tenta1/ExerciseTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("again");
+        list.add("world");
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
