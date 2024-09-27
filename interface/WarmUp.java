802bddb4ff715a0b306a74e63e9e456d6601edf6
andreljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2024-09-27 11:38:59 +0200

    132

---
+++ interface/WarmUp.java
@@ -0,0 +1,7 @@
+import se.yrgo.iop.*;
+
+public class WarmUp {
+    public static void main(String[] args) {
+        Misc.doStuff();
+    }
+}
---
+++ interface/se/yrgo/iop/Misc.java
@@ -0,0 +1,7 @@
+package se.yrgo.iop;
+
+public class Misc {
+    public static void doStuff() {
+        System.out.println("doing stuff");
+    }
+}
