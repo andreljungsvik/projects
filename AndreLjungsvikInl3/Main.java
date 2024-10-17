03dced950a330355e97e01d4242b7c72613c2f75
andreljungsvik <andre.ljungsvik@gmail.com>
2024-10-17 15:15:51 +0200

    123

--- AndreLjungsvikInl3/Main.java
+++ AndreLjungsvikInl3/Main.java
@@ -1,25 +1,36 @@
 import java.util.*;
 
+// klass för att köra spargris programmet
 public class Main {
     public static void main(String[] args) {
         PiggyBank piggyBank = new PiggyBank();
         try (Scanner input = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("Vilket mynt vill du spara? ange 0 för att avsluta");
-                try {
-                    int coin = input.nextInt();
-                    if (coin == 0) { // input 0 avslutar och visar mynt statistik
-                        System.out.println("Du har sparat " + piggyBank.getCoinCount() + " till ett totalt värde av "
-                                + piggyBank.getCoinSum() + " kr");
-                        break;
-                    }
-                    piggyBank.addCoins(coin);
-                } catch (InputMismatchException e) { // hanterar ogiltig inmatning
-                    System.out.println("Ogiltig mynt! Spargrisen accepterar endast svenska mynt (1, 2, 5, 10).");
-                    input.next(); // rensa felaktig inmatning
-                }
-            }
+            runPiggyBank(input, piggyBank);
+        }
+    }
 
+    // metod för körning och utskrifter
+    private static void runPiggyBank(Scanner input, PiggyBank piggyBank) {
+        while (true) {
+            System.out.println("Vilket mynt vill du spara? ange 0 för att avsluta");
+
+            try {
+                int coin = input.nextInt();
+
+                if (coin == 0) { // input 0 avslutar och visar mynt statistik
+                    System.out.println("Du har sparat " + piggyBank.getCoinCount() + " till ett totalt värde av "
+                            + piggyBank.getCoinSum() + " kr");
+                    break;
+                }
+                boolean isValidCoin = piggyBank.addCoins(coin); // kollar om myntet var giltigt eller ej
+
+                if (!isValidCoin) { // ifall myntet var ogiltigt
+                    System.out.println("Ogiltig mynt! Spargrisen accepterar endast svenska mynt (1, 2, 5, 10).");
+                }
+            } catch (InputMismatchException e) { // hanterar ogiltig inmatning
+                System.out.println("Ogiltig mynt! Spargrisen accepterar endast svenska mynt (1, 2, 5, 10).");
+                input.next(); // rensa felaktig inmatning
+            }
         }
     }
 }
--- AndreLjungsvikInl3/PiggyBank.java
+++ AndreLjungsvikInl3/PiggyBank.java
@@ -1,17 +1,20 @@
+// Den här klassen reprensenterar en spargris
+
 public class PiggyBank {
     private int coinCount = 0;
     private int coinSum = 0;
     final private int[] validCoins = { 1, 2, 5, 10 }; // tillåtna mynt. oförändbar
 
-    // metoden lägger till mynt ifall det är giltligt
-    public void addCoins(int coin) {
+    // lägger till mynt och retunerar true eller false beroende på giltigthet
+    public boolean addCoins(int coin) {
         for (int validCoin : validCoins) {
             if (coin == validCoin) {
                 coinCount++;
                 coinSum += coin;
-                break;
+                return true;
             }
         }
+        return false;
     }
 
     public int getCoinCount() {
