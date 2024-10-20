20616ee89c6f158b177254b5c79d6b900e1f7be6
andreljungsvik <andre.ljungsvik@gmail.com>
2024-10-20 15:35:46 +0200

    bajs

--- exceptions/ExerciseFour.java
+++ exceptions/ExerciseFour.java
@@ -1,9 +1,7 @@
 public class ExerciseFour {
     public static void main(String[] args) {
-        try {
         int[] bajs = { 1, 2, 3 };
         System.out.println(bajs[3]);
-        }
-
+    
     }
 }
