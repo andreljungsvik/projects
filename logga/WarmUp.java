5fa1df0076785789c34cfdf0adec182d39ff8177
andreljungsvik <andre.ljungsvik@gmail.com>
2024-10-23 14:40:20 +0200

    2

---
+++ logga/WarmUp.java
@@ -0,0 +1,9 @@
+import org.slf4j.*;
+
+public class WarmUp {
+    private final static Logger logger = LoggerFactory.getLogger(WarmUp.class);
+
+    public static void main(String[] args) {
+        logger.info("Logging some really nice piece of information!");
+    }
+}
