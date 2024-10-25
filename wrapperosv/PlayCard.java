5656d577e837aaee9eabfc99db8b7142d553b2b5
andreljungsvik <andre.ljungsvik@gmail.com>
2024-10-25 14:16:09 +0200

    123

---
+++ wrapperosv/PlayCard.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class PlayCard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
+
+        while(!deck.isEmpty()) {
+            System.out.println("Gissa färg på nästa kort: ");
+            
+        }
+    }
+}
