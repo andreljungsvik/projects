4aa071e1919384075b67e648c96dbdde8daaafb8
andreljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2024-11-12 11:28:52 +0100

    123

--- streamlambda/FindJava.java
+++ streamlambda/FindJava.java
@@ -5,11 +5,11 @@ import java.util.stream.*;
 
 public class FindJava {
     public static void main(String[] args) {
-        try(Stream<Path> filesWalk = Files.walk(Paths.get(args[0]))){
+        try (Stream<Path> filesWalk = Files.walk(Paths.get(args[0]))) {
             List<String> javaFiles = filesWalk.filter(Files::isRegularFile)
-            .map(f -> f.toString())
-            .filter(f -> f.endsWith(".java"))
-            .collect(Collectors.toList());
+                    .map(f -> f.toString())
+                    .filter(f -> f.endsWith(".java"))
+                    .collect(Collectors.toList());
 
             javaFiles.forEach(System.out::println);
         } catch (IOException e) {
